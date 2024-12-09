8d305a80c18ed3d5a902b3431ff9fce1df4b3719
kt <kthobbs@mail.ubc.ca>
2024-12-09 15:02:29 +0100

    output paths generalised

--- cloudrf/to_gpkg.R
+++ cloudrf/to_gpkg.R
@@ -4,17 +4,35 @@ library('stringr')
 library('dplyr')
 library('mapview')
 
-files = list.files("cloudrf/output/")
+# Set-up ----
+# country to run analysis
+country_list <- list.dirs(path = 'reach/populations/', recursive = TRUE, full.names = TRUE) %>% basename()
+print(country_list[2:length(country_list)])
 
+country <- readline(prompt = "Enter the country name or type 'all' for all: ")
+print(paste("Radio reach estimates for: ,", country))
+
+
+# cloudrf raw output files
+files = list.files(sprintf("cloudrf/output/raw/%s/"), country)
+
+# if (country == 'all'){
+  # TODO: add option to iterate through all
+#   country <- country_list[2:length(country_list)]
+# }
+
+# Test
 file <- files[3]
-for (file in files[4:length(files)]) {
+
+# Iterate ----
+for (file in files[8:length(files)]) {
   
   # Get file name
   fname <- str_extract(file, "[A-Za-z](.+)(?=\\.shp\\.zip)")
   print(fname)
 
   # Read CloudRF output file
-  cloudrf_data <- st_read(here("cloudrf", "output", file), promote_to_multi = FALSE, quiet = TRUE)
+  cloudrf_data <- st_read(here("cloudrf", "output", "gpkg", country, file), promote_to_multi = FALSE, quiet = TRUE)
   
 
   # simple_data <- st_simplify(valid_data, dTolerance = 0.01)
@@ -35,11 +53,13 @@ for (file in files[4:length(files)]) {
     multipolygon <- st_make_valid(multipolygon)
   }
   
-  # Repeat??  # Check for invalid geometries
+  # Repeat as first pass returns geometry collection - # Check for invalid geometries
   invalid_geometries <- !st_is_valid(multipolygon)
   if (any(invalid_geometries)) {
-    cat("Invalid geometries found. Applying st_make_valid().\n")
+    cat("Invalid geometries found. Applying st_make_valid() and snapping floating point differences.\n")
+    # Make valid and eliminate floating point differences by snapping geometry
     multipolygon <- st_make_valid(multipolygon)
+    multipolygon <- st_set_precision(multipolygon, 1e-7)
   }
   
   # Dissolve
@@ -49,6 +69,6 @@ for (file in files[4:length(files)]) {
   # mapview(polygon)
   
   # Export as gpkg
-  st_write(polygon, sprintf("cloudrf/output/gpkg/%s.gpkg", fname), layer = fname, append=FALSE)
+  st_write(polygon, sprintf("cloudrf/output/gpkg/%s/%s.gpkg", country, fname), layer = fname, append=FALSE)
 
 }
