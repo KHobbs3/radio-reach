84b4065579f24295abc54a2b48c00871cbe6336e
kt <kthobbs@mail.ubc.ca>
2024-12-16 16:25:06 +0100

    terra R package for I/O

--- cloudrf/to_gpkg.R
+++ cloudrf/to_gpkg.R
@@ -3,6 +3,7 @@ library('here')
 library('stringr')
 library('dplyr')
 library('mapview')
+library('terra')
 
 # Set-up ----
 # # country to run analysis
@@ -29,57 +30,22 @@ for (arg in args) {
 country <- arg_list[["country"]]
 
 # cloudrf raw output files
-files = list.files(sprintf("cloudrf/output/raw/%s/"), country)
-
-
-# Test
-file <- files[3]
+files = list.files(here("cloudrf", sprintf("output/raw/%s/", country)))
+print(files)
 
 # Iterate ----
-for (file in files[8:length(files)]) {
+for (file in files[1:length(files)]) {
   
   # Get file name
-  fname <- str_extract(file, "[A-Za-z](.+)(?=\\.shp\\.zip)")
+  fname <- str_extract(file, "[A-Za-z](.+)(?=\\.gpkg)")
   print(fname)
 
   # Read CloudRF output file
-  cloudrf_data <- st_read(here("cloudrf", "output", "raw", country, file), promote_to_multi = FALSE, quiet = TRUE)
-  
+  # cloudrf_data <- st_read(here("cloudrf", "output", "raw", country, file), promote_to_multi = FALSE, quiet = TRUE)
+  cloudrf_data <- rast(here("cloudrf", "output", "raw", country, file))
 
-  # simple_data <- st_simplify(valid_data, dTolerance = 0.01)
-  
-  # Deduplicate
-  # TODO: add if-statement
-  deduplicated <- cloudrf_data %>%
-    distinct(geometry, .keep_all = TRUE)
-  
-  # Merge
-  multipolygon <- st_combine(deduplicated)
-  
-  
-  # Check for invalid geometries
-  invalid_geometries <- !st_is_valid(multipolygon)
-  if (any(invalid_geometries)) {
-    cat("Invalid geometries found. Applying st_make_valid().\n")
-    multipolygon <- st_make_valid(multipolygon)
-  }
-  
-  # Repeat as first pass returns geometry collection - # Check for invalid geometries
-  invalid_geometries <- !st_is_valid(multipolygon)
-  if (any(invalid_geometries)) {
-    cat("Invalid geometries found. Applying st_make_valid() and snapping floating point differences.\n")
-    # Make valid and eliminate floating point differences by snapping geometry
-    multipolygon <- st_make_valid(multipolygon)
-    multipolygon <- st_set_precision(multipolygon, 1e-7)
-  }
-  
-  # Dissolve
-  polygon <- st_union(multipolygon)
-  
-  # Check
-  # mapview(polygon)
-  
-  # Export as gpkg
-  st_write(polygon, sprintf("cloudrf/output/gpkg/%s/%s.gpkg", country, fname), layer = fname, append=FALSE)
+  # Save the polygons as a GeoPackage
+  terra::writeRaster(cloudrf_data, here("cloudrf", sprintf("output/gpkg/%s/%s.gpkg", country, fname)),
+              overwrite = TRUE)
 
 }
