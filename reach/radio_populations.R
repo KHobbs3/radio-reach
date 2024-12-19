45346784437415f357617bcae01db77f30372d23
kt <kthobbs@mail.ubc.ca>
2024-12-19 15:59:14 +0100

    Raster station extended and resampled to align with pop raster.

--- reach/radio_populations.R
+++ reach/radio_populations.R
@@ -1,153 +1,145 @@
-# Script to select facilities within radio bounds
-# Load required libraries
 library(sf)
-library(dbscan)
+library(terra)
 library(dplyr)
-library(raster)
-library(geosphere)
 library(here)
 library(mapview)
-library(data.table)
 library(openxlsx)
 library(stringr)
-library(terra)
 
-# DEFINE VARIABLES ------
-# projection for epsg:102022
+# Set-up ----
+options(warn = -1)  # Suppress warnings
+
+# Set projection
 proj <- "+proj=aea +lat_0=0 +lon_0=25 +lat_1=20 +lat_2=-23 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs +type=crs"
-kilometres <- c(1,2,3,4,5)
 
-# country for analysis
-country_list <- list.dirs(path = 'reach/populations/', recursive = TRUE, full.names = TRUE) %>% basename()
-print(country_list[2:length(country_list)])
+# Get the command-line arguments to identify country for analysis
+args <- commandArgs(trailingOnly = TRUE)
 
-country <- readline(prompt = "Enter the country name or type 'all' for all: ")
-print(paste("Radio reach estimates for:", country))
-
-if (country == 'all'){
-  # TODO: add option to iterate through all
-  country <- country_list[2:length(country_list)]
+# Parse the keyword arguments
+arg_list <- list()
+for (arg in args) {
+  if (grepl("--", arg)) {
+    key_value <- strsplit(arg, "=")[[1]]
+    if (length(key_value) == 2) {
+      arg_list[[sub("--", "", key_value[1])]] <- key_value[2]
+    }
+  }
 }
 
-# prompt user which data source to use for health centres
-hf_source <- readline(prompt = "HDX or DHIS2:")
+# Access the keyword arguments ----
+country <- arg_list[["country"]]
+facility_source <- arg_list[['fs']]
 
-
-# READ DATA ----
 # Read in all possible stations
 filepath = sprintf('cloudrf/output/gpkg/%s', country)
 gpkg_files <- list.files(path = filepath, pattern = "\\.gpkg$", full.names = TRUE, recursive = TRUE)
 
 # Read all .gpkg files into a list of sf objects
-sf_list <- lapply(gpkg_files, function(file) {
-  sf_object <- st_read(file)
-  sf_object$source_file <- basename(file)
-  return(sf_object)
+station_list <- lapply(gpkg_files, function(file) {
+  radio <- rast(file)
+  radio$source_file <- basename(file)
+  return(radio)
 })
 
-# Read in the population raster
-population_raster <- rast(list.files(sprintf("reach/populations/%s", country),
-                                       pattern = "\\.tif$", 
-                                       full.names = TRUE))
+# Read population raster and reproject it
+print("Reprojecting population raster...")
+population_raster <- rast(list.files(here("reach", "populations", country), full.names = T)[1])
+population_raster <- project(population_raster, proj)
 
-# Set CRS 
-population_raster <- project(population_raster, crs = proj)
+# Read health facilities (ensure CRS matches)
+print(sprintf("Reading health facilities data from %s...", facility_source))
+hf <- st_read(list.files(here("reach", "health-centres", facility_source, country), full.names = T)[1])
+hf_proj <- st_transform(hf, proj)
 
-
-# Read in health facilities points
-hf_files <- list.files(here("reach", "health-centres", hf_source, country),
-                       full.names=T)
-hf <- st_read(hf_files[1])
-
-
-# DEFINE FUNCTIONS ----
-# Population coverage function
-pop_coverage <- function(polygon) {
-  
-  # Crop the raster to the polygon to speed up processing
-  cropped_raster <- crop(population_raster, polygon)
-  
-  # Mask the raster to only include values within the polygon
-  masked_raster <- mask(cropped_raster, polygon)
-  
-  # Calculate the total population within the polygon (sum non-NA values)
-  total_population <- global(masked_raster, fun = "sum", na.rm = TRUE)[1, 1]
-  
+# Function to calculate population coverage ----
+pop_coverage <- function(raster, polygon) {
+  raster <- terra::mask(raster, polygon)
+  total_population <- terra::global(raster, "sum", na.rm = TRUE)[1, 1]
   return(total_population)
 }
 
+# Set-up table for export ----
+population_data <- tibble(source_file = character(),
+                          population_coverage = numeric(),
+                          radio_coverage = numeric(),
+                          population_proportion = numeric(),
+                          kilometre = integer(),
+                          station_name = character()
+                          )
 
-# ITERATOR ----
-for (km in kilometres){
-  print(km)
-  
-  # Reproject hf's
-  hf_proj <- st_transform(hf, proj)
-  
-  # Buffer points ----
-  print(paste("Adding dissolved buffer:", km))
-  hf_buffer <- hf_proj %>%
-                  st_buffer(dist = km*1000) %>%
-                  st_union() %>%
-                  st_sf()
-  
+# Iterator ----
+# Loop for each distance in km
+for (km in c(1, 2, 3, 4, 5)) {
+  print(sprintf("----- KM: %s -----", km))
+  hf_buffer <- hf_proj %>% 
+    st_buffer(dist = km * 1000) %>% 
+    st_union() %>%
+    st_sf()
 
-  table_list <- list() 
-  
-  # Iterate through all radio stations and create a list of 
-  # facilities *within*
-  
-  for (i in seq(length(sf_list))) {
+  for (i in seq_along(station_list)) {
+    station <- station_list[[i]]
     
-    station_name <-  str_sub(gsub('\\.gpkg$',"", 
-                                basename(gpkg_files[i])),1,31)
+    # Verbosity: get station name
+    sname = str_sub(basename(gpkg_files[i]), 1, 31)
+    print(sprintf("Station %s of %s: %s", i, length(station_list), sname))
     
-    # Verbosity
-    print(paste(sprintf("%s of %s", i, length(sf_list)), 
-                station_name))
+    # Reproject station
+    station <- project(station, proj)
     
-    # read station
-    station <- sf_list[[i]]
+    # Clip station bounds to the buffered facility locations
+    cropped_polygons <- terra::crop(station, terra::ext(hf_buffer))
     
-    # Reproject the station to match the facility polygon layer
-    station <- st_transform(station, proj)
+    # Fill NA's that are out of the facility buffer bounds
+    masked_polygons <- terra::mask(cropped_polygons, hf_buffer)
     
-    # Using intersects because people slightly outside radio bounds likely move in bounds
-    # !!! N.B. differs from python script
-    intersect_polygons <- st_intersection(hf_buffer, station)
-    print(paste("Number of facility clusters within radio bounds:", nrow(intersect_polygons)))
     
+    if (!is.null(masked_polygons)) {
+      # Force align extent of masked_polygons to population_raster's extent
+      print("Extending masked facilities to population raster...")
+      masked_polygons_aligned <- extend(masked_polygons, ext(population_raster))
+      
+      print("Extending radio station to population raster...")
+      station_raster_aligned <- extend(station, ext(population_raster))
+      
+      # Resample masked_polygons to match population_raster
+      print("Resampling masked facilities to population raster...")
+      masked_polygons_aligned <- resample(masked_polygons_aligned, population_raster, method = "bilinear")
 
-    if (length(unlist(intersect_polygons)) > 0) {
-      # Get station name and append as new column
-      intersect_polygons$source_file <- gsub('\\.gpkg$',"", basename(gpkg_files[i]))
-      intersect_polygons <- intersect_polygons %>%
-                                              mutate(
-                                                     population_coverage = pop_coverage(intersect_polygons),
-                                                     radio_coverage = pop_coverage(station), # Calculate population coverage of station and add it to the buffer population dataframe
-                                                     kilometre = km,
-                                                     population_prop = population_coverage/station_population_R,
-                                                     station_name = station_name)
-                                          }
-    
-    # Append to table
-    table_list[[station_name]] <- intersect_polygons
+      print("Resampling radio stations to population raster...")
+      station_raster_aligned <- resample(station_raster_aligned, population_raster, method = "bilinear")
+      
+      # Population coverage calculations ----
+      print("Calculating X-km population coverage..")
+      population_coverage <- pop_coverage(population_raster, masked_polygons_aligned)
 
-    # Export for each radio station
-    st_write(intersect_polygons, dsn = sprintf("reach/intermediates/%s/%s_%gkm.gpkg", country, station_name, km),
-             layer = sprintf("%s", station_name), driver = "GPKG", delete_dsn = TRUE)
-    
+      print("Calculating radio station population coverage..")
+      radio_coverage <- pop_coverage(population_raster, station_raster_aligned)
+      
+      # Collect data for each radio station
+      population_data <- population_data %>%
+        add_row(
+          source_file = basename(gpkg_files[i]),
+          population_coverage = population_coverage,
+          radio_coverage = radio_coverage,
+          population_proportion = population_coverage / radio_coverage,
+          kilometre = km,
+          station_name = sname
+        )
+      
+      # Export as GeoPackage
+      print("Exporting intermediate output..")
+      polygons <- as.polygons(masked_polygons_aligned)  # Convert SpatRaster to polygons
+      writeVector(polygons, filename = sprintf("reach/intermediates/%s/%s_%gkm.gpkg", country, sname, km),
+               overwrite = TRUE)
+    }
   }
-  
 }
 
-# Remove geometry for table export
-table_list_flat <- lapply(table_list, st_drop_geometry)
+# Export summary output ----
+print("Exporting summary output...")
+write.csv(population_data, here("reach", "output", country, sprintf("%s_summary_reach.csv", country)), row.names = F)
+print("Fin.")
 
-# Export radio station summary for x-KM
-write.xlsx(table_list_flat, file = sprintf("reach/output/%s/buffer_populations_%s.xlsx", country, country))
-
-# check
-mapview::mapview( list(intersect_polygons, station),
-                  col.regions = list("blue", "red", "yellow"))
+# Check
+mapview(polygons)
 
