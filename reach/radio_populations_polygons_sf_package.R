1ebc780a916da0037058dc6de3d621e5b7415219
kt <kthobbs@mail.ubc.ca>
2025-05-29 09:01:08 -0700

    Active script for population estimation; custom reprojections.

--- reach/radio_populations_polygons_sf_package.R
+++ reach/radio_populations_polygons_sf_package.R
@@ -11,6 +11,11 @@ library(raster)
 # Set-up ----
 options(warn = -1)  # Suppress warnings
 
+# Variable selection
+country <- "nigeria"
+station_source <- "fem_nigeria/qgis_polygonization"
+facility_source <- "HDX"
+km <- 5
 
 # Set-up function for calculating population coverage
 pop_coverage <- function (population_raster, polygon){
@@ -20,22 +25,21 @@ pop_coverage <- function (population_raster, polygon){
                                })
 }
 
-country <- "nigeria"
-station_source <- "fem_nigeria/qgis_polygonization/"
-facility_source <- "HDX"
+# Set projection to equal area projection for country
+proj_df <- read.csv('reach/country_projections.csv')
+area_proj <- proj_df %>% filter(country_name == country) %>% pull(proj)
+print(area_proj)
 
 # Read population raster and reproject it
-print("Reprojecting population raster...")
+print("Reading population raster...")
 population_raster <- raster(list.files(here("reach", "populations", country), full.names = T, pattern = "*.tif$")[1])
-# population_raster <- projectRaster(population_raster, crs= proj)
-# Set projection
-proj <- crs(population_raster)#"+proj=sinu +lat_0=0 +lon_0=25 +lat_1=20 +lat_2=-23 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs +type=crs"
 
 
 # Read in all possible stations
 print("Reading radio GPKGs...")
 filepath = sprintf(here('cloudrf/output/gpkg/%s/%s/'), country, station_source)
 gpkg_files <- list.files(path = filepath, pattern = "\\.gpkg$", full.names = TRUE, recursive = F)
+
 
 # Read all .gpkg files into a list of sf objects
 station_list <- lapply(gpkg_files, function(file) {
@@ -46,6 +50,9 @@ station_list <- lapply(gpkg_files, function(file) {
   
   # add column
   radio_polygon$source_file <- basename(file)
+  
+  # assign crs
+  # radio_polygon <- st_set_crs(radio_polygon, "EPSG:4326")
   
   # reproject
   radio_polygon <- st_transform(radio_polygon, crs(population_raster))
@@ -58,8 +65,20 @@ station_list <- lapply(gpkg_files, function(file) {
 # Read health facilities (ensure CRS matches)
 print(sprintf("Reading health facilities data from %s...", facility_source))
 hf <- st_read(list.files(here("reach", "health-centres", facility_source, country), pattern = '*.geojson$', full.names = T)[1])
-hf_proj <- st_transform(hf, proj)
 
+# Reproject from degrees to area projection
+hf_proj <- st_transform(hf, area_proj)
+
+# Add x-KM buffer in an areal projection
+print(sprintf("----- KM: %s -----", km))
+
+hf_buffer <- hf_proj %>% 
+  st_buffer(dist = km * 1000) %>% 
+  st_union() %>%
+  st_sf()
+
+# Reproject health facilities to the popgrid degrees proj
+hf_buffer_deg <- st_transform(hf_buffer, crs(population_raster))
 
 # Set-up table for export ----
 population_data <- tibble(source_file = character(),
@@ -74,76 +93,69 @@ population_data <- tibble(source_file = character(),
 errors = list()
 
 # Loop for each distance in km
-for (km in c(5)) {
-  print(sprintf("----- KM: %s -----", km))
-  
-  hf_buffer <- hf_proj %>% 
-    st_buffer(dist = km * 1000) %>% 
-    st_union()
-
-  for (i in seq_along(station_list)) {
+for (i in seq_along(station_list)) {
     station <- station_list[[i]]
     
     # Verbosity: get station name
     sname = str_sub(basename(gpkg_files[i]), 1, 31)
     print(sprintf("Station %s of %s: %s", i, length(station_list), sname))
-    
-    # # Reproject station
-    # station <- st_transform(station, proj)
-    
+
     # Clip station bounds to the buffered facility locations
-    cropped_polygons <- st_intersection(station, hf_buffer)
+    cropped_polygons <- st_intersection(station, hf_buffer_deg)
     
-    if (!is.null(cropped_polygons)) {
+    if (!nrow(cropped_polygons) == 0) {
       # Population coverage calculations ----
       print("Calculating X-km population coverage..")
       population_coverage <- pop_coverage(population_raster, cropped_polygons)
-
-      print("Calculating radio station population coverage..")
-      radio_coverage <- pop_coverage(population_raster, station)
+    } else {
+      population_coverage <- 0
+    }
+    
+    print("Calculating radio station population coverage..")
+    radio_coverage <- pop_coverage(population_raster, station)
+    
+    # Ensure polygon covers a population
+    if (length(radio_coverage) == 0){
+      len <- length(errors)
       
-      # Ensure polygon covers a population
-      if (length(population_coverage) == 0){
-        len <- length(errors)
-        
-        #append value to end of list
-        errors[[len+1]] <- sname
-      } else {
-        # Collect data for each radio station
-        population_data <- population_data %>%
-          add_row(
-            source_file = basename(gpkg_files[i]),
-            population_coverage = population_coverage,
-            radio_coverage = radio_coverage,
-            population_proportion = population_coverage / radio_coverage,
-            kilometre = km,
-            station_name = sname
-          )
+      # append value to end of list
+      errors[[len+1]] <- sname
+      
+    } else {
+      
+      # Collect data for each radio station
+      population_data <- population_data %>%
+        add_row(
+          source_file = sname,
+          population_coverage = population_coverage,
+          radio_coverage = radio_coverage,
+          population_proportion = population_coverage / radio_coverage,
+          kilometre = km,
+          station_name = sname
+        )
       }
       
-      # Export as GeoPackage
+      # Export
       print("Exporting intermediate output..")
       dir.create(file.path(here(sprintf("reach/intermediates/%s/", country))))
       st_write(cropped_polygons, dsn = here(sprintf("reach/intermediates/%s/polygons_%s_%gkm.gpkg", country, sname, km)),
                append = F)
     }
-  }
-}
+
 
 # Export summary output ----
 print("Exporting summary output...")
 dir.create(file.path(here("reach", "output", country)))
-write.csv(population_data, here("reach", "output", country, sprintf("%s_summary_reach_sf.csv", country)), row.names = F)
+write.csv(population_data, here("reach", "output", country, sprintf("%s_%s_summary_reach_sf_orig_proj.csv", country, station_source)), row.names = F)
 capture.output(summary(errors), file = here("reach", "output", country, "error_no_populaiton.txt"))
 print("Fin.")
 
 # Checks
-library(mapview)
 
 # mapview(hf_buffer, col.regions = "blue") +
   mapview(population_raster, col.regions = "green",
           na.color = NA) +
+    # mapview(cropped_polygons, col.regions = 'blue') +
     mapview(station_list[[1]], col.regions = "red") +
-    mapview(station_list[[2]], col.regions = "red") 
-
-  
+    mapview(hf_buffer_deg, col.regions = "red") 
+    
